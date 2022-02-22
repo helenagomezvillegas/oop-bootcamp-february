2b3a82b1a39a63953f38ad660ac79b5384fac362
Helena <helena.gomez@thoughtworks.com>
2022-02-22 15:13:27 +0100

    add compare centimeters and meters

--- src/main/java/oop/Converter.java
+++ src/main/java/oop/Converter.java
@@ -1,5 +1,7 @@
 package oop;
 
+import java.awt.geom.Arc2D;
+
 public class Converter {
 
     public static final double FOOT_TO_METERS_CONVERSION_FACTORS = 0.3048;
@@ -9,6 +11,10 @@ public class Converter {
     }
 
     public boolean compareInchWithYards(double inches, double yards) {
-        return Double.compare(yards, 0.0278 * inches) == 0;
+        return Double.compare(yards, 0.0278 * inches ) == 0;
+    }
+
+    public boolean compareMetersWithCentimeters(double meters, double centimeters) {
+        return Double.compare(centimeters, 100 * meters) == 0;
     }
 }
--- src/test/java/oop/ConverterTest.java
+++ src/test/java/oop/ConverterTest.java
@@ -5,28 +5,35 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class ConverterTest {
+    private Converter converter = new Converter();
     @Test
     public void itShouldConvertFootToMeters() {
-        Converter converter = new Converter();
         assertEquals(converter.calculateMeters(1), 0.3048);
     }
     @Test
     public void itShouldConvertTwoFeetToMeters() {
-        Converter converter = new Converter();
+
         assertEquals(converter.calculateMeters(2), 0.6096);
     }
 
     @Test
     public void itShouldCompareInchesWithYards() {
-        Converter converter = new Converter();
-
         assertTrue(converter.compareInchWithYards(5.0, 0.1390));
     }
 
     @Test
     public void itShouldReturnFalseWhenComparingNotEquivalentInchAndYardValue() {
-        Converter converter = new Converter();
 
         assertFalse(converter.compareInchWithYards(2.0, 0.0278));
+    }
+    @Test
+    public void itShouldReturnTrueWhenComparingMetersWithCentimeters() {
+        assertTrue(converter.compareMetersWithCentimeters(2.0, 200.0));
+
+    }
+
+    @Test
+    public void itShouldReturnFalseWhenComparingMetersWithCentimeters() {
+        assertFalse(converter.compareMetersWithCentimeters(3.0, 200.0));
     }
 }
